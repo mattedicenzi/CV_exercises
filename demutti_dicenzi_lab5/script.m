7460b7968031863971d799878a8ab7f3b797cea7
mattedicenzi <32385330+mattedicenzi@users.noreply.github.com>
2020-04-18 15:49:53 +0200

    commenti

--- demutti_dicenzi_lab5/script.m
+++ demutti_dicenzi_lab5/script.m
@@ -1,4 +1,16 @@
+%% Lab 5 - NCC-based segmentation and Harris corner detection
 %Dicenzi Matteo 4342944
 %Demutti Marco  4389233
 
-%% exercise 1
+%% Exercise 1: NCC-based segmentation
+
+% Read all 6 RGB images
+img1_rgb=imread('ur_c_s_03a_01_L_0376.png', 'png');
+img2_rgb=imread('ur_c_s_03a_01_L_0377.png', 'png');
+img3_rgb=imread('ur_c_s_03a_01_L_0378.png', 'png');
+img4_rgb=imread('ur_c_s_03a_01_L_0379.png', 'png');
+img5_rgb=imread('ur_c_s_03a_01_L_0380.png', 'png');
+img6_rgb=imread('ur_c_s_03a_01_L_0381.png', 'png');
+
+
+%% Exercise 2: Harris corner detection
