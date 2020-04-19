652cedad0844c27c2ff46082b71793702b3a0c12
marcodemutti <marcodemutti@gmail.com>
2020-04-19 20:27:37 +0200

    Aggiunta funzione corner_detection

--- demutti_dicenzi_lab5/script.m
+++ demutti_dicenzi_lab5/script.m
@@ -173,32 +173,7 @@ figure, imagesc(g), colormap gray, title('Gaussian filter')
 Sx2 = conv2(Ix2, g, 'same'); Sy2 = conv2(Iy2, g, 'same'); Sxy = conv2(Ixy, g, 'same');
 
 % Corner detection
-[rr,cc]=size(Sx2);
-corner_reg = zeros(rr,cc);
-R_map = zeros(rr,cc); %R_map colorness map
-k = 0.05;
-
-for ii=1:rr
-    for jj=1:cc
-        % define at each pixel x,y the matrix M
-        M = [Sx2(ii,jj), Sxy(ii,jj); Sxy(ii,jj), Sy2(ii,jj)];
-        % compute the response of the detector at each pixel
-        R = det(M) - k*(trace(M).^2);
-        R_map(ii,jj) = R;
-    end
-end
-
-% Compute max value of R map
-M = max(R_map, [], 'all');
-
-% Threshold for the corner regions using M
-for ii=1:rr
-    for jj=1:cc
-        if R_map(ii,jj) > 0.3*M
-            corner_reg(ii,jj) = 1;
-        end
-    end
-end
+[R_map, corner_reg] = corner_detection(Sx2, Sxy, Sy2);
 
 % Get the centroids of the blobs in the corner regions map
 props = regionprops(logical(corner_reg), 'Centroid');
