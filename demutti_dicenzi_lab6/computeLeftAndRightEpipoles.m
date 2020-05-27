518b79fdfeec63502863a241acec991dbb9705d9
marcodemutti <32385267+marcodemutti@users.noreply.github.com>
2020-05-27 17:57:32 +0200

    funzione epipoli

---
+++ demutti_dicenzi_lab6/computeLeftAndRightEpipoles.m
@@ -0,0 +1,14 @@
+function [left_ep_cartesian, right_ep_cartesian] = computeLeftAndRightEpipoles(F)
+
+% SVD decomposition of F
+[U,W,V] = svd(F);
+
+% Compute left and right epipoles as the right and left null space of F
+% i.e. last column of U and V respectively
+left_ep_homogeneous = U(:,end);
+right_ep_homogeneous = V(:,end);
+
+% Write the two epipoles in cartesian coordinates
+left_ep_cartesian = [left_ep_homogeneous(1)/left_ep_homogeneous(3), left_ep_homogeneous(2)/left_ep_homogeneous(3)];
+right_ep_cartesian = [right_ep_homogeneous(1)/right_ep_homogeneous(3), right_ep_homogeneous(2)/right_ep_homogeneous(3)];
+end
--- demutti_dicenzi_lab6/script.m
+++ demutti_dicenzi_lab6/script.m
@@ -14,41 +14,35 @@ mire1 = imread('.\Mire\Mire1.pgm');
 mire2 = imread('.\Mire\Mire2.pgm');
 
 % Load the two sets P1 and P2 and store them in a matrix 3xN
-P1 = load('.\Mire\Mire1.points');
-P1_hom = [P1'; ones(1,size(P1,1))];
+P1_mire = load('.\Mire\Mire1.points');
+P1_mire_hom = [P1_mire'; ones(1,size(P1_mire,1))];
 
-P2 = load('.\Mire\Mire2.points');
-P2_hom = [P2'; ones(1,size(P2,1))];
+P2_mire = load('.\Mire\Mire2.points');
+P2_mire_hom = [P2_mire'; ones(1,size(P2_mire,1))];
 
 % Estimate F from P1 and P2 (using the 2 functions)
-F1 = EightPointsAlgorithm(P1_hom, P2_hom);
-F2 = EightPointsAlgorithmN(P1_hom, P2_hom);
+F1_mire = EightPointsAlgorithm(P1_mire_hom, P2_mire_hom);
+F2_mire = EightPointsAlgorithmN(P1_mire_hom, P2_mire_hom);
 
 % Check that X2'*F1*X1 = 0 for each X1 in P1 and X2 in P2
-constraintMireF1 = zeros(1,size(P1_hom,2));
-for i=1:size(P1_hom,2)
-    constraintMireF1(i) = P2_hom(:,i)' * F1 * P1_hom(:,i);
+constraintMireF1 = zeros(1,size(P1_mire_hom,2));
+for i=1:size(P1_mire_hom,2)
+    constraintMireF1(i) = P2_mire_hom(:,i)' * F1_mire * P1_mire_hom(:,i);
 end
 
 % Check that X2'*F2*X1 = 0 for each X1 in P1 and X2 in P2
-constraintMireF2 = zeros(1,size(P1_hom,2));
-for i=1:size(P1_hom,2)
-    constraintMireF2(i) = P2_hom(:,i)' * F2 * P1_hom(:,i);
+constraintMireF2 = zeros(1,size(P1_mire_hom,2));
+for i=1:size(P1_mire_hom,2)
+    constraintMireF2(i) = P2_mire_hom(:,i)' * F2_mire * P1_mire_hom(:,i);
 end
 
-% visualizeEpipolarLines(mire1, mire2, F1, P1, P2);
-% visualizeEpipolarLines(mire1, mire2, F2, P1, P2);
+% visualizeEpipolarLines(mire1, mire2, F1_mire, P1_mire, P2_mire);
+% visualizeEpipolarLines(mire1, mire2, F2_mire, P1_mire, P2_mire);
 
-[U,W,V] = svd(F2);
-
-left_ep_F2 = U(:,end);
-left_ep_F2_non_hom = [left_ep_F2(1)/left_ep_F2(3), left_ep_F2(2)/left_ep_F2(3)];
-
-right_ep_F2 = V(:,end);
-right_ep_F2_non_hom = [right_ep_F2(1)/right_ep_F2(3), right_ep_F2(2)/right_ep_F2(3)];
-
-figure, imshow(mire1), hold on;
-plot(left_ep_F2_non_hom, 's');
+[left_ep_cartesian_F2_mire, right_ep_cartesian_F2_mire] = computeLeftAndRightEpipoles(F2_mire);
+figure, imagesc(mire1), colormap gray;
+figure;
+plot(left_ep_cartesian_F2_mire(1), left_ep_cartesian_F2_mire(2), 's');
 
 %% 'Rubik' set of images
 
@@ -57,27 +51,27 @@ rubik1 = imread('.\Rubik\Rubik1.pgm');
 rubik2 = imread('.\Rubik\Rubik2.pgm');
 
 % Load the two sets P1 and P2 and store them in a matrix 3xN
-P1 = load('.\Rubik\Rubik1.points');
-P1_hom = [P1'; ones(1,size(P1,1))];
+P1_rubik = load('.\Rubik\Rubik1.points');
+P1_rubik_hom = [P1_rubik'; ones(1,size(P1_rubik,1))];
 
-P2 = load('.\Rubik\Rubik2.points');
-P2_hom = [P2'; ones(1,size(P2,1))];
+P2_rubik = load('.\Rubik\Rubik2.points');
+P2_rubik_hom = [P2_rubik'; ones(1,size(P2_rubik,1))];
 
 % Estimate F from P1 and P2 (using the 2 functions)
-F1 = EightPointsAlgorithm(P1_hom, P2_hom);
-F2 = EightPointsAlgorithmN(P1_hom, P2_hom);
+F1_rubik = EightPointsAlgorithm(P1_rubik_hom, P2_rubik_hom);
+F2_rubik = EightPointsAlgorithmN(P1_rubik_hom, P2_rubik_hom);
 
 % Check that X2'*F1*X1 = 0 for each X1 in P1 and X2 in P2
-constraintRubikF1 = zeros(1,size(P1_hom,2));
-for i=1:size(P1_hom,2)
-    constraintRubikF1(i) = P2_hom(:,i)' * F1 * P1_hom(:,i);
+constraintRubikF1 = zeros(1,size(P1_rubik_hom,2));
+for i=1:size(P1_rubik_hom,2)
+    constraintRubikF1(i) = P2_rubik_hom(:,i)' * F1_rubik * P1_rubik_hom(:,i);
 end
 
 % Check that X2'*F2*X1 = 0 for each X1 in P1 and X2 in P2
-constraintRubikF2 = zeros(1,size(P1_hom,2));
-for i=1:size(P1_hom,2)
-    constraintRubikF2(i) = P2_hom(:,i)' * F2 * P1_hom(:,i);
+constraintRubikF2 = zeros(1,size(P1_rubik_hom,2));
+for i=1:size(P1_rubik_hom,2)
+    constraintRubikF2(i) = P2_rubik_hom(:,i)' * F2_rubik * P1_rubik_hom(:,i);
 end
 
-% visualizeEpipolarLines(rubik1, rubik2, F1, P1, P2);
-% visualizeEpipolarLines(rubik1, rubik2, F2, P1, P2);
+% visualizeEpipolarLines(rubik1, rubik2, F1_rubik, P1_rubik, P2_rubik);
+% visualizeEpipolarLines(rubik1, rubik2, F2_rubik, P1_rubik, P2_rubik);
